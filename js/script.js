ce1c7a461a04bab92998d900339957fc31668502
adib nur <adiblubis123@gmail.com>
2024-10-28 11:21:52 +0700

    menambahkan initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// event pada saat link di klik
+$('.page-scroll').on('click', function(e){
+
+	//ambil isi href
+	var tujuan = $(this).attr('href');
+	// tangkap elemen ybs
+	var elemenTujuan = $(tujuan);
+
+
+	//pindahkan scroll
+	$('body').animate({
+		scrolltop: elemenTujuan.offset().top - 50
+	}, 1250,'easeInOutExpo');
+
+
+	e.preventDefault();
+
+
+});
